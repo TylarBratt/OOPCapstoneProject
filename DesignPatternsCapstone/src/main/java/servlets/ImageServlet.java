3089e799c35d83a0c1100b01be8658a2ceb44a23
Philip Bemis <50244170+PhilBeta@users.noreply.github.com>
2022-06-15 20:18:17 -0400

    Implemented basic image server
    Any requests to /image will return the image data of the file reference by "src" paramater.

--- DesignPatternsCapstone/src/main/java/servlets/ImageServlet.java
+++ DesignPatternsCapstone/src/main/java/servlets/ImageServlet.java
@@ -1,6 +1,9 @@
 package servlets;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
@@ -14,8 +17,35 @@ public class ImageServlet extends HttpServlet{
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		ServletContext sc = getServletContext();
-		//try (InputStream is = sc.getRes)
+		//Image source file is specified in src parameter.
+		String src = req.getParameter("src");
+		
+		ServletContext cntx= req.getServletContext();
+		  //SOLUTION OBTAINED FROM https://stackoverflow.com/questions/8623709/output-an-image-file-from-a-servlet
+	      // Get the absolute path of the image
+	      String filename = cntx.getRealPath("images/"+src);
+	      // retrieve mimeType dynamically
+	      String mime = cntx.getMimeType(filename);
+	      if (mime == null) {
+	        resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+	        return;
+	      }
+
+	      resp.setContentType(mime);
+	      File file = new File(filename);
+	      resp.setContentLength((int)file.length());
+
+	      FileInputStream in = new FileInputStream(file);
+	      OutputStream out = resp.getOutputStream();
+
+	      // Copy the contents of the file to the output stream
+	       byte[] buf = new byte[1024];
+	       int count = 0;
+	       while ((count = in.read(buf)) >= 0) {
+	         out.write(buf, 0, count);
+	      }
+	    out.close();
+	    in.close();
 	}
 	
 }
