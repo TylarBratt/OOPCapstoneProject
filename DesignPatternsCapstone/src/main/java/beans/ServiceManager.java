a2cc50b86e24a6e69270373af8c0f14a83d2f234
Philip Bemis <50244170+PhilBeta@users.noreply.github.com>
2022-07-11 19:52:59 -0400

    More comments for clarity

--- DesignPatternsCapstone/src/main/java/beans/ServiceManager.java
+++ DesignPatternsCapstone/src/main/java/beans/ServiceManager.java
@@ -10,6 +10,10 @@ import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
 
+/**
+ * Service Manager
+ * Responsible for starting and stopping any background tasks that run independently from the servlets.
+ */
 @WebListener
 public class ServiceManager implements ServletContextListener {
 	Database dbConnection = null;
@@ -22,6 +26,8 @@ public class ServiceManager implements ServletContextListener {
 	@Override
 	public void contextInitialized(ServletContextEvent event) {
 		dbConnection = new Database();
+		
+		//Use the scheduler to start background tasks here....
 		
 		System.out.println("Starting finished auction processor service..");
 		scheduler.scheduleAtFixedRate(new ProcessFinishedAuctionsTask(dbConnection), 0, interval, TimeUnit.MILLISECONDS);
@@ -36,7 +42,6 @@ public class ServiceManager implements ServletContextListener {
 		try {
 			scheduler.awaitTermination(3, TimeUnit.SECONDS);
 		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
