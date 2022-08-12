48bc2446e5f5c8851333a58825dc07a2af6ee5dc
Philip Bemis <50244170+PhilBeta@users.noreply.github.com>
2022-08-12 12:19:49 -0400

    Migrated remaining html from Servlet to JSP

--- DesignPatternsCapstone/src/main/java/servlets/AuctionDetailsController.java
+++ DesignPatternsCapstone/src/main/java/servlets/AuctionDetailsController.java
@@ -1,14 +1,9 @@
 package servlets;
 
-import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
-
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import beans.Auction;
 import beans.Bid;
 import beans.Product;
@@ -27,22 +22,6 @@ public class AuctionDetailsController extends JSPController {
 		super("auction-details.jsp", true, true);	
 	}
 	
-	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// If user is not logged in, redirect to login screen.
-		boolean isLoggedIn = req.getSession().getAttribute("user") != null;
-		if (isLoginRequired && !isLoggedIn) 
-			resp.sendRedirect(req.getContextPath() + "/login");
-		else {
-			//Attach database attribute to the request.
-			initializeData(req);
-			
-			//Forward the request to the associated JSP file.
-			RequestDispatcher dispatcher = req.getRequestDispatcher("/jsp/"+jspPath);
-	        dispatcher.forward(req, resp);
-		}
-
-	}
 	
 	/*
 	 * This is where you initialize any data that you want passed into the JSP. 
@@ -55,89 +34,29 @@ public class AuctionDetailsController extends JSPController {
 		Product product = database.getProductWithID(auction.productID);
 		
 		StringBuilder table = new StringBuilder();
-		List<Bid> bids = database.getBidsForAuction(id);
-		if (bids.size() > 0) {	
-			//Add a row of info for each bid..
-			table.append("<table>");
-			table.append("<tr><td>Amount</td><td>Time</td><td>User</td>");
-			StringBuilder tableBody = new StringBuilder();
-			for (Bid bid : bids) 
-				tableBody.append("<tr>"
-						+ "<td>"
-						+ bid.amount
-						+ "</td>"
-						+ "<td>"
-						+ bid.time
-						+ "</td>"
-						+ "<td>"
-						+ database.getUser(bid.userID).getMaskedUserName()
-						+ "</td>");
-			
-			table.append(tableBody.toString() + "</table>");
-		}
-		else
-			table.append("<p>No Bids</p>");
 		
 		User user = database.getUser(auction.ownerID);
 		
+		List<Bid> bids = database.getBidsForAuction(id);
 		
+		//Build list of masked usernames for each bid..
+		List<String> bidderNames = new ArrayList<>();
+		for (Bid bid : bids) {
+			if (bid != null)
+				bidderNames.add(database.getUser(bid.userID).getMaskedUserName());
+			else
+				bidderNames.add(null);
+		}
+		
+		req.setAttribute("Bids", bids);
+		req.setAttribute("BidderNames", bidderNames);
 		req.setAttribute("Auction", auction);
 		req.setAttribute("Product", product);
 		req.setAttribute("Table", table.toString());
 		req.setAttribute("Owner", user);
 	}
-	
-	
 }
-        	
-        	/*
-        	@Override
-	public String getBodyHTML(HttpServletRequest req) {
-		//Get the auction ID from the URL parameter list
-		long auctionID = Long.parseLong(req.getParameter("aid"));
-		//Get the auction from the database using the auction ID.
-		Auction auction = database.getAuctionWithID(auctionID);
-		//Get the product being auctioned.
-		Product product = database.getProductWithID(auction.productID);
-		
-		//Include product image
-		String productImage = readFileText(new ProductIconHTMLAdapter(product));
-		
-		//Include table of bids
-		StringBuilder table = new StringBuilder();
-		List<Bid> bids = database.getBidsForAuction(auctionID);
-		if (bids.size() > 0) {	
-			//Add a row of info for each bid..
-			StringBuilder tableBody = new StringBuilder();
-			for (Bid bid : bids) 
-				tableBody.append(readFileText("html/bid-table-row.html", 
-						bid.amount, 
-						bid.time, 
-						database.getUser(bid.userID).getMaskedUserName()));
-			
-			table.append(readFileText("html/bid-table.html", tableBody));
-		}
-		else
-			table.append("<p>No Bids</p>");
-		
-		
-		return readFileText("html/auction-details.html", 
-				productImage,
-				table,
-				auction.startPrice, 
-				auction.startDate,
-				auction.getEndDate(),
-				auction.getTimeRemaining(database.getCurrentTimestamp()),
-				database.getUser(auction.ownerID).userName);
-	
-	}
-	
-
-@Override
-public String getActiveNavbarItem() {
-	return "home";
-}
-*/
+        
 
 
 
